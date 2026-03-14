97822375fdb02820ba3de2e7614d65affe242835
diagmindtw <diagmindtw@gmail.com>
2026-03-14 20:50:21 +0800

    Commit from TeleportHQ

---
+++ src/views/google-pixel21.js
@@ -0,0 +1,88 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './google-pixel21.css'
+
+const GooglePixel21 = (props) => {
+  return (
+    <div className="google-pixel21-container">
+      <Helmet>
+        <title>exported project</title>
+        <meta property="og:title" content="exported project" />
+        <link rel="canonical" href="https://untitled-gp0s2i.teleporthq.site/" />
+        <meta
+          property="og:url"
+          content="https://untitled-gp0s2i.teleporthq.site/"
+        />
+      </Helmet>
+      <div className="google-pixel21-thq-google-pixel21-elm">
+        <div className="google-pixel21-thq-my-head-elm">
+          <div className="google-pixel21-thq-page-audio-elm">
+            <div className="google-pixel21-thq-content-elm1">
+              <div className="google-pixel21-thq-statelayer-elm1">
+                <img
+                  alt="IconI114"
+                  src="/iconi114-5mlf.svg"
+                  className="google-pixel21-thq-icon-elm1"
+                />
+              </div>
+            </div>
+          </div>
+          <button className="google-pixel21-thq-main-action-buttons-elm">
+            <div className="google-pixel21-thq-user-text-input-elm">
+              <div className="google-pixel21-thq-statelayer-elm2">
+                <img
+                  alt="IconI139"
+                  src="/iconi139-nmkd.svg"
+                  className="google-pixel21-thq-icon-elm2"
+                />
+                <span className="google-pixel21-thq-text-elm1 M3labellarge">
+                  Label
+                </span>
+              </div>
+            </div>
+            <div className="google-pixel21-thq-text-to-ary-elm">
+              <div className="google-pixel21-thq-statelayer-elm3">
+                <img
+                  alt="IconI139"
+                  src="/iconi139-e22a.svg"
+                  className="google-pixel21-thq-icon-elm3"
+                />
+                <span className="google-pixel21-thq-text-elm2 M3labellarge">
+                  Label
+                </span>
+              </div>
+            </div>
+            <div className="google-pixel21-thq-master-seg-elm">
+              <div className="google-pixel21-thq-statelayer-elm4">
+                <img
+                  alt="IconI139"
+                  src="/iconi139-s2it.svg"
+                  className="google-pixel21-thq-icon-elm4"
+                />
+                <span className="google-pixel21-thq-text-elm3 M3labellarge">
+                  Label
+                </span>
+              </div>
+            </div>
+          </button>
+          <div className="google-pixel21-thq-page-log-elm">
+            <div className="google-pixel21-thq-content-elm2">
+              <div className="google-pixel21-thq-statelayer-elm5">
+                <img
+                  alt="IconI116"
+                  src="/iconi116-yuyi.svg"
+                  className="google-pixel21-thq-icon-elm5"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="google-pixel21-thq-frame1-elm"></div>
+      </div>
+    </div>
+  )
+}
+
+export default GooglePixel21
